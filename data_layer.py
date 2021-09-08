bdbec6efc98b3817dd67defcbe53115742e043c1
Alessandro Giacche <alejack9@users.noreply.github.com>
2021-09-08 19:20:28 +0200

    added data layer

---
+++ data_layer.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+
+# function to load the dataset
+def load_data():
+    # not using "index_col='_id.$oid'" because it makes the dataframe messier
+    data = pd.read_csv('dataset/signalfingerprints.csv')
+    data['target'] = data['family'] + '.' + data['location']
+    # drop unwanted columns
+    # data = data.drop(['_id.$oid', 'Unnamed: 0', '__v', 'location', 'family', 'timestamp', 'device'], axis=1)
+    data = data.drop(['_id.$oid', 'Unnamed: 0', '__v'], axis=1)
+    X = data.iloc[:, :-1]
+    y = data['target']
+    num_classes = len(np.unique(y))
+
+    return X, y, num_classes
