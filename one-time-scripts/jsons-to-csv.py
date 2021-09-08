4445df610da0085e8616c2c98b1188d3da7139ac
Alessandro Giacche <alejack9@users.noreply.github.com>
2021-09-08 19:20:16 +0200

    json to csv converter

---
+++ one-time-scripts/jsons-to-csv.py
@@ -0,0 +1,14 @@
+import os
+import json
+import pandas as pd
+from pandas.io.json import json_normalize
+
+data = pd.DataFrame()
+for file in os.listdir('./jsons'):
+    with open('./jsons/' + file) as f:
+        d = json.load(f)
+
+    print('./jsons/' + file)
+    data = pd.concat([data, pd.DataFrame(json_normalize(d))])
+data.to_csv('signalfingerprints.csv')
+print(len(data.columns))
