fa775c1bfa197aa4df346dd04ede25b5d14bce58
Alessandro Giacche <alejack9@users.noreply.github.com>
2021-09-08 19:21:18 +0200

    grouped all together

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-# This is a sample Python script.
-# This is a sample Python script.
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+import data_layer
+import preprocessing
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    X, y, num_classes = data_layer.load_data()
+    preprocessing.priori_analysis(X, y)
+    # print(X)
+    # print('-------------')
+    # print(y)
+    # print('-------------')
+    # print(num_classes)
+    # print(np.unique(y))
