8c9bb23cb2da8991600aa9a7415b1c68d2434daf
andiub97 <43032657+andiub97@users.noreply.github.com>
2021-10-14 19:16:03 +0200

    add comments to models_config

--- model_runner.py
+++ model_runner.py
@@ -13,32 +13,36 @@ from models_config import models
 def get_rank1_info(result, attribute):
     return result.loc[result['rank_test_score'] == 1][attribute].values[0]
 
-
+# return models 
 def retrieve_best_models(X_train, y_train, fs, use_saved_if_available, save_models, models_dir):
     best_models = {}
     for est_name, est, params in models:
         est_name = est_name + fs
         filename = est_name + '.joblib'
 
+        # load the model if it was saved before 
         if use_saved_if_available and path.exists(path.join(models_dir, filename)):
-            # load the model
             print(f"Saved model found: {est_name}")
+            # save model binary in best model pipeline
             best_models[est_name] = {'pipeline': load(path.join(models_dir, filename))}
+            #put csv model in result
             result = pd.read_csv(path.join(models_dir, "csvs", est_name + ".csv"))
         else:
-            # cross-validate: since cv=10, the validation set is 10% of the whole train dataset
+            # cross-validate: since cv=10, the validation set is 10% of the whole train dataset(80% of the whole dataset)
             result, current_pipeline = run_crossvalidation(X_train, y_train, est, params, cv=10)
             best_models[est_name] = {'pipeline': current_pipeline}
             if save_models:
                 # save the model and the associated info
                 dump(best_models[est_name]['pipeline'], path.join(models_dir, filename))
+                # put csv model in result
                 result.to_csv(path.join(models_dir, "csvs", est_name + ".csv"))
 
-        # retrieve metadata of the best model
+        # retrieve metadata of the model that has rank_test_score equal to 1
         attributes = ["mean_train_score", "mean_test_score", "mean_fit_time", "mean_score_time"]
         for attribute in attributes:
             best_models[est_name][attribute] = get_rank1_info(result, attribute)
         best_models[est_name]['_all_results'] = result
+    # return for each best model result and pipeline 
     return best_models
 
 
@@ -50,7 +54,7 @@ def run_crossvalidation(X_trainval, y_trainval, clf, params, cv=10, verbose=True
         RandomOverSampler(random_state=42, sampling_strategy='minority'),
         StandardScaler(),
         clf)
-
+    # GridSearchCV approach for fitting models
     grid_search = GridSearchCV(pipeline, params, cv=cv, verbose=10 if verbose else 0, n_jobs=-1,
                                return_train_score=True)
     grid_search.fit(X_trainval, y_trainval)
