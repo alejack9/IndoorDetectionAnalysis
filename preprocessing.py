c793bb3bd0fc9d54690cfa58d8d428ad425f73e1
andiub97 <43032657+andiub97@users.noreply.github.com>
2021-10-14 19:16:33 +0200

    add comments to preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -2,12 +2,12 @@ import visualization
 import pandas as pd
 
 
-# return columns which start by prefix specified in parameter
+# return columns, from dataset, X which start by the prefix specified in parameter
 def get_dataset_prefix(X, prefix):
     return X[[col for col in X if col.startswith(prefix)]]
 
 
-#
+# plot feature's missing values series starting with same prefix, grouped by the sensor specified in parameter
 def missing_values(X, prefix, sensor):
     X_pref = get_dataset_prefix(X, prefix)
     # plot the percentage of missing values for each feature
@@ -21,11 +21,12 @@ def features_distribution(X, prefix, sensor):
     # plot the percentage of missing values for each feature
     visualization.plot_density_all(get_dataset_prefix(X, prefix), sensor)
 
-
+# plot features missing values and distribution of wifi, bluetooth and gps signals
 def priori_analysis(X, y):
     families = X.groupby('family')
     for i, d in enumerate([families.get_group(x) for x in families.groups]):
         d = d.dropna(axis=1, how='all')
+
         missing_values(d, 'signals.wifi', list(families.groups.keys())[i].capitalize() + ' - WIFI')
         missing_values(d, 'signals.bluetooth', list(families.groups.keys())[i].capitalize() + ' - BLUETOOTH')
         missing_values(d, 'gps.', list(families.groups.keys())[i].capitalize() + ' - GPS')
@@ -44,19 +45,24 @@ def priori_analysis(X, y):
     visualization.plot_class_distribution(y)
     visualization.plot_all()
 
-
+# create datasets starting from the main dataset and return his three subsets and their size
 def create_datasets(X):
+    # drop dataset's common columns
     base = X.drop(['location', 'family', 'timestamp', 'device'], axis=1)
+
+    # one base dataset, one subset excluding gps features and one subset including only gps features
     X_subsets = [base,
                  base.drop(['gps.latitude', 'gps.longitude', 'gps.altitude', 'gps.accuracy'], axis=1),
                  base[['gps.latitude', 'gps.longitude', 'gps.altitude', 'gps.accuracy']]
                  ]
 
+    # subsets' columns number
     subsets_sizes = ["_" + str(len(df.columns)) for df in X_subsets]
 
     return X_subsets, subsets_sizes
 
-
+# replace nan values from train and test set by filling them with -100 values since it's the minimum value
+# of wireless signals power in theory 
 def remove_nan(X_train, X_test):
     X_train = pd.DataFrame(X_train).fillna(-100, inplace=False).to_numpy()
     if X_test is not None:
